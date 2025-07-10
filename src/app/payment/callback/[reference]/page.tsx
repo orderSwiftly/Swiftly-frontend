91526199d36f27609936c6c27cab2464e09274e0
VictorDev <ememevictor08@gmail.com>
2025-07-10 17:23:39 +0100

    Fix payment callback params by separating client/server components 2

--- src/app/payment/callback/[reference]/page.tsx
+++ src/app/payment/callback/[reference]/page.tsx
@@ -1,6 +1,6 @@
-import dynamic from 'next/dynamic';
+// src/app/payment/callback/[reference]/page.tsx
 
-const PaymentCallbackPage = dynamic(() => import('./paymentCallback'), { ssr: false });
+import PaymentCallbackPage from './paymentCallback'; // This will be a client component
 
 export default function Page({ params }: { params: { reference: string } }) {
   return <PaymentCallbackPage reference={params.reference} />;
--- src/app/payment/callback/[reference]/paymentCallback.tsx
+++ src/app/payment/callback/[reference]/paymentCallback.tsx
@@ -1,3 +1,5 @@
+// src/app/payment/callback/[reference]/PaymentCallbackPage.tsx
+
 'use client';
 
 import { useEffect, useState } from 'react';
