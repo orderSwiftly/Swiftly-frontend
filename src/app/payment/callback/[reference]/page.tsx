1ee93bd9b9e54f283c0861c9f73236d600716681
VictorDev <ememevictor08@gmail.com>
2025-07-11 15:38:29 +0100

    callback fix 3

--- src/app/payment/callback/[reference]/page.tsx
+++ src/app/payment/callback/[reference]/page.tsx
@@ -2,6 +2,6 @@
 
 import PaymentCallbackClient from './paymentCallback';
 
-export default function Page({ params }: { readonly params: { readonly reference: string } }) {
+export default function Page({ params }: { readonly params: { readonly reference: string } }) { // this won't work
   return <PaymentCallbackClient reference={params.reference} />;
 }
